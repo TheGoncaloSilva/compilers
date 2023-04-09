a34a867af621ae4ca54f77c408f747af0a128850
Gonçalo Silva <43342715+TheGoncaloSilva@users.noreply.github.com>
2023-04-09 09:30:48 +0100

    cleanup

--- practical/class_1/src/ex_1_2_xtra.java
+++
@@ -1,9 +0,0 @@
-/*
- * Program used to improved the calculator and allow true
- *  calculations with priority
- */
-public class ex_1_2_xtra {
-    public static void main(String[] args){
-
-    }
-}
